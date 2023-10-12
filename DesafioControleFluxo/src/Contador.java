662c066e3dd2e1dc084e62df3489039fa332d22c
¨wemenson¨ <wemensongomes11@gmail.com>
2023-10-11 23:41:09 -0300

    projeto finalizado

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,53 +1,46 @@
 
-    import java.util.Scanner ;
+import java.util.Scanner;
 
-public class Contador{
+public class Contador {
 
     public static void main(String[] args) {
 
-            try (Scanner terminal = new Scanner(System.in)) {
-                System.out.println("Digite o primeiro numero (Parametro) : ");
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro numero (Parametro) : ");
 
-                int parametroUm = terminal.nextInt();
+            int parametroUm = terminal.nextInt();
 
-                System.out.println("Digite o primeiro numero (Parametro) : ");
-                int parametroDois = terminal.nextInt();
-      
-                try {
-   
+            System.out.println("Digite o primeiro numero (Parametro) : ");
+            int parametroDois = terminal.nextInt();
+
+            try {
+
                 contar(parametroUm, parametroDois);
 
+            } catch (ParametrosInvalidosException e) {
 
-   }
-   catch(ParametrosInvalidosException e) {
-
-
-
-      System.out.println("Mensagem de error: " + e.getMessage());
-
-
-
-    }
+                System.out.println("Mensagem de error: " + e.getMessage());
             }
-}
-static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-
-if(parametroUm > parametroDois) {
-    throw new ParametrosInvalidosException("O Segundo nunero deve ser maior que o primeiro ");
-
-}
-
-for (int i = parametroUm; i <= parametroDois; i++) {
-    System.out.println("Imprimindo o numero " + i);
-    
-}
-
-    //necessario criar uma classe ParametrosInvalidosException
-}
-    class ParametrosInvalidosException extends Exception{
-        public ParametrosInvalidosException(String message) { 
-            super (message);
-            
         }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O Segundo numero deve ser maior que o primeiro ");
+
+        }
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println("Imprimindo o numero " + i);
+
+        }
+
+        // necessario criar uma classe ParametrosInvalidosException
+    }
 }
-}
+class ParametrosInvalidosException extends Exception {
+        public ParametrosInvalidosException(String message) {
+            super(message);
+
+        }
+    }
